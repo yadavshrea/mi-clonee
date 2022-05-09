eaef9f20a81e1dda4bf12341412db08b4e1ff159
yadavshrea <shreyayadav1607@gmail.com>
2022-05-09 22:26:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Prenavbar from './components/Prenavbar';
+
+const App = () => {
+  
+  return(
+    <div>
+     <Prenavbar></Prenavbar>
+    </div>
+  )
+}
+export default App;
